bec6d4527b5cbca63b261cd8037becb0d91de389
Lola <lolahef@gmail.com>
2019-02-03 18:52:57 -0500

    Added route to user dropdown; doesn't like that I'm putting a Link

--- frontend/src/components/ProfileHeader/UserDropdown.js
+++ frontend/src/components/ProfileHeader/UserDropdown.js
@@ -8,7 +8,7 @@
 
 //-- Dependencies --------------------------------
 import React from 'react'
-import { Dropdown } from 'semantic-ui-react'
+import { Dropdown, Header, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 // Need to figure out how to route in a semantic ui dropdown, hesitant to wrap whole dropdown in a <Link>
 import { Mutation } from 'react-apollo';
@@ -28,16 +28,31 @@ const UsersDropdown = ({ userId, users }) => {
   //Takes users from props and maps over each user 
   const userList = filtered.map(user => {
     return {
+      key: user.id,
       text: user.name,
-      value: user.id,
-      icon: 'user',
-      className: 'profile_dropList'
+      className: 'profile_dropList',
+      content: 
+      <Mutation mutation={MUTATION_FRIEND_PROFILE}>
+        {setFriendId => (
+          <Link 
+            to={`/friends/${user.id}`}
+            onClick={() => setFriendId({ variables: { id: user.id }})}>
+            <Header>
+              <Image 
+                circular
+                src={user.pictureUrl ? user.pictureUrl : 'https://jazzyacres.com/wp-content/uploads/2016/09/File-Sep-15-4-04-02-PM.jpeg'}
+              />
+              {user.name}
+            </Header>
+          </Link>
+        )}
+      </Mutation>
     };
   });
 
   return(
-    <Mutation mutation={MUTATION_FRIEND_PROFILE}>
-      {setFriendId => (
+    // <Mutation mutation={MUTATION_FRIEND_PROFILE}>
+    //   {setFriendId => (
         <Dropdown
         placeholder='Search for friends'
         floating
@@ -46,13 +61,12 @@ const UsersDropdown = ({ userId, users }) => {
         icon='search'
         options={userList}
         className='profile_userDropDown'
-        onChange={(e, data) => {
-          setFriendId({ variables: { id: data.value }});
-          // Router.pushRoute('friends', {id: data.value} )
-        }}
+        // onChange={(e, data) => {
+        //   setFriendId({ variables: { id: data.value }});
+        // }}
         />
-      )}
-    </Mutation>
+    //   )}
+    // </Mutation>
   );
 }
 export default UsersDropdown;
